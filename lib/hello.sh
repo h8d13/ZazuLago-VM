12e211e9f2903ee117d555f9a4dc64f0f5da0ef7
HADEON <52324046+h8d13@users.noreply.github.com>
2025-02-28 17:01:12 +0100

    Add files via upload

---
+++ lib/hello.sh
@@ -0,0 +1,2 @@
+./hedgen2c e hello.png helogoencrypted.png 420 && ./hedgen2c d helogoencrypted.png helogogodecrypted.png 420 && ./hedgen2c d helogoencrypted.png helogogocorruptedonpurpose.png 6969
+
