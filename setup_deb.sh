e781e264f50b2b94a745c7d58558d8735ae57381
HADEON <52324046+h8d13@users.noreply.github.com>
2025-05-21 15:23:40 +0200

    Update and rename setup.sh to setup_deb.sh

--- setup_deb.sh
+++ setup_deb.sh
@@ -1,10 +1,5 @@
 # Main needed
 apt install qemu-system qemu-utils qemu-kvm libvirt-daemon-system libvirt-clients
 
-# d for isos, c for disks
-mkdir d
-mkdir c
-
 # Info
 lsmod | grep kvm
-egrep -c '(vmx|svm)' /proc/cpuinfo
